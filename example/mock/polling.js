b7e9d8887749b209f315eda5dd185fb3d6ae4ab2
JiangWeixian <saber_wei@163.com>
2019-10-15 11:32:05 +0800

    chore: polling example

--- example/mock/polling.js
+++ example/mock/polling.js
@@ -18,4 +18,4 @@ const proxy = {
   },
 };
 
-export default delay(proxy, 2000);
+export default delay(proxy, 100);
---
+++ example/src/pages/polling/index.tsx
@@ -0,0 +1,19 @@
+import React, { useState, useCallback } from 'react';
+import { usePolling } from 'use-heart-beat';
+import { Button } from 'antd';
+
+import { api } from '@/api';
+
+export default () => {
+  const [id, setId] = useState<string>('1');
+  const fetch = useCallback(async () => {
+    return api.polling.request(id);
+  }, [id]);
+  const { data } = usePolling<number>({ id, api: fetch, delay: 1000 });
+  return (
+    <>
+      <span>{data}</span>
+      <Button onClick={() => setId(id === '1' ? '2' : '1')}>改变ID</Button>
+    </>
+  );
+};
